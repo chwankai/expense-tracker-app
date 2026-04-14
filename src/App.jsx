da989054f831964666f6766dc32f224986f310c2
chwankai <111103988+chwankai@users.noreply.github.com>
2026-04-15 00:01:38 +0800

    Added basic structure of the tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,93 @@
+// The root of the system
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const updateCount = () => {
+    setCount(count+1)
+  }
+
+  const isCK = true
+
+  function header(){
+    return 'Expense Tracker'
+  }
+  
+  function calTotal(amount){
+    if(amount < 50){
+      return <span>You are saving money!</span>
+    }
+    else if(amount < 300){
+      return <span>Please control your budget!</span>
+    }
+    else{
+      return <span>Stop spending!</span>
+    }
+  }
+
+  const transaction = [
+    {id: 1001, trns:'RM 200.00'},
+    {id: 1002, trns:'RM 100.00'},
+    {id: 1003, trns:'RM 300.00'},
+  ]
+
+  const clickHandler = (e) => {
+    console.log("Button work in progress!", e)
+  }
+  const displayName = (name, e) => {
+    console.log("Button clicked by ", name, e)
+  }
+
+  function SubmitButton(){
+    return <button>click me</button>
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <div>
+      {/* if isCK is true display CK else display Guest */}
+      <h1 id="header">{isCK ? "CK's " : "Guest's "}{header()}</h1>
+      {/* secret message for CK only */}
+      {/* && -> if want true only */}
+      {isCK && <h2>Welcome back</h2>}
+      <div className='container' style={{color:'red'}}>
+        <h2>Balance</h2>
+        <p>RM 100.00</p>
+      </div>
+      <div className='container'>
+        <h2>Income</h2>
+        <p>RM 300.00</p>
+      </div>
+      <div className='container'>
+        <h2>Expense</h2>
+        <p>RM 200.00</p>
+      </div>
+      <div className="container">
+        <h2>Transaction</h2>
+        <ul>
+          {/* loop the transaction list */}
+          {/* must have unique key for each row */}
+          {transaction.map(item =><li key ={item.id}>{item.trns}</li>)}
+        </ul>
+      </div>
+      <hr />
+      <h2>Testing Farm </h2>
+      <hr />
+      {/* call function and pass parameter */}
+      <span>Reminder: </span>{calTotal(700)}
+      <br />
+      <button onClick={clickHandler}>Add New Records</button>
+      {/* If want pass value must use arrow function here */}
+      <button onClick={(e) => displayName('CK', e)}>Press to show name</button>
+      
+      {/* auto update count */}
+      <button onClick={updateCount}>{count}</button>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+// The entry point of the system
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
